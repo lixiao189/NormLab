b8f4bce549f61e0ba2656fd023851db62a4683c7
Node Sans <node189@icloud.com>
2022-04-17 21:20:42 +0800

    压缩模块完成

--- src/normlab.py
+++ src/normlab.py
@@ -1,14 +1,26 @@
 import pathlib
 
+from extractor import UnZip
+
 
 class NormLab:
-    def __init__(self, homeworksPath: str) -> None:
+    def __init__(self, homeworks_path: str) -> None:
         # 读取 id
-        self.__lab_id = pathlib.Path(homeworksPath).name.split("-")[0]
+        self.__lab_id = pathlib.Path(homeworks_path).name.split("-")[0]
+        self.__result_dir = "../result"  # 结果输出路径
 
-    def get_lab_id(self) -> int:
+    def get_lab_id(self) -> str:
         return self.__lab_id
 
 
 if __name__ == '__main__':
-    pass
+    sourceHomework = UnZip(
+        "/Users/node/Developer/Python/NormLab/data/Lab03-JUnit for Unit Test.zip",
+        "../temp"
+    )
+
+    with UnZip(
+        "/Users/node/Developer/Python/NormLab/data/Lab03-JUnit for Unit Test.zip",
+        "../temp"
+    ) as unzip:
+        unzip.extract()
---
+++ test/test_extractor.py
@@ -0,0 +1,44 @@
+import shutil
+import os
+
+import extractor
+
+
+def test_unzip():
+    """
+    测试解压 zip 文件, 检查某个学生的文件是否被解压出来
+    """
+    source_file_path = "../data/Lab03-JUnit for Unit Test.zip"
+    target_dir_path = "../temp"
+
+    a_student_file_name = "L201926630134-HOMWEYETSUROMARTIN.zip"
+
+    with extractor.UnZip(source_file_path, target_dir_path) as unzip:
+        unzip.extract()
+
+        for root, dirs, files in os.walk(target_dir_path):
+            assert a_student_file_name in files
+            break
+
+        # 删除临时文件
+        shutil.rmtree(target_dir_path)
+
+
+def test_unrar():
+    """
+    测试解压 rar 文件
+    """
+    source_file_path = "../data/test.rar"
+    target_dir_path = "../temp"
+
+    file_dir_name = "test"
+
+    with extractor.UnRar(source_file_path, target_dir_path) as unrar:
+        unrar.extract()
+
+        for root, dirs, files in os.walk(target_dir_path):
+            assert file_dir_name in dirs
+            break
+
+        # 删除临时文件
+        shutil.rmtree(target_dir_path)
