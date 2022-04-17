4a167ec23c394ded74ee66dd2bb44ed758919838
Node Sans <node189@icloud.com>
2022-04-17 23:37:36 +0800

    添加读取学生列表模块

--- src/normlab.py
+++ src/normlab.py
@@ -1,26 +1,111 @@
+import csv
+import os
 import pathlib
+import typing
 
-from extractor import UnZip
+import extractor
+
+
+class Student:
+    def __init__(self, stu_id: str, full_name: str, short_name: str) -> None:
+        self.__stu_id = stu_id
+        self.__full_name = full_name
+        self.__short_name = short_name
+
+    # getters
+    def get_stu_id(self) -> str:
+        return self.__stu_id
+
+    def get_full_name(self) -> str:
+        return self.__full_name
+
+    def get_short_name(self) -> str:
+        return self.__short_name
 
 
 class NormLab:
-    def __init__(self, homeworks_path: str) -> None:
-        # 读取 id
-        self.__lab_id = pathlib.Path(homeworks_path).name.split("-")[0]
-        self.__result_dir = "../result"  # 结果输出路径
+    def __init__(self, homeworks_path: str, result_dir: str, students_list_path: str = "") -> None:
+        self.__students: typing.Dict[str, Student] = dict()
+        self.__homeworks_path = homeworks_path
+        self.__lab_id = pathlib.Path(self.__homeworks_path).name.split("-")[0]
+        self.__result_dir = result_dir + "/" + self.__lab_id  # 结果输出路径
+
+        # 初始化学生列表
+        self.read_students_list(students_list_path)
+
+    def read_students_list(self, students_list_path) -> None:
+        """
+        从 csv 中读取学生信息
+        """
+        with open(students_list_path) as f:
+            csv_reader = csv.reader(f)
+
+            # 读取文件头
+            _ = next(csv_reader)
+
+            # 读取学生信息
+            for row in csv_reader:
+                self.__students[row[0]] = Student(row[0], row[1], row[2])
+
+    def extract_source_homework(self) -> None:
+        """
+        解压作业
+        """
+        homeworks_extractor = None
+        homeworks_path_obj = pathlib.Path(self.__homeworks_path)
+
+        if homeworks_path_obj.suffix == ".zip":
+            homeworks_extractor = extractor.UnZip
+        elif homeworks_path_obj.suffix == ".rar":
+            homeworks_extractor = extractor.UnRar
+
+        # 解压原始文件
+        with homeworks_extractor(self.__homeworks_path, self.__result_dir) as e:
+            e.extract()
+
+        # 对每个学生的文件进行解压
+        for root, dirs, files in os.walk(self.__result_dir):
+            for file in files:
+                # 解压当前文件
+                path_obj = pathlib.Path(os.path.join(root, file))
+                if path_obj.suffix == ".zip":
+                    homeworks_extractor = extractor.UnZip
+                elif path_obj.suffix == ".rar":
+                    homeworks_extractor = extractor.UnRar
+
+                with homeworks_extractor(os.path.join(root, file),
+                                         root + "/" + path_obj.name.split(".")[0]):
+                    homeworks_extractor.extract()
+
+                os.remove(path_obj.name)
+
+    # getters
+    def get_students(self) -> typing.Dict[str, Student]:
+        return self.__students
+
+    def get_homeworks_path(self) -> str:
+        """
+        获取作业源文件路径
+        """
+        return self.__homeworks_path
 
     def get_lab_id(self) -> str:
+        """
+        获取 lab id
+        """
         return self.__lab_id
+
+    def get_result_dir(self) -> str:
+        """
+        获取输出结果路径
+        """
+        return self.__result_dir
 
 
 if __name__ == '__main__':
-    sourceHomework = UnZip(
-        "/Users/node/Developer/Python/NormLab/data/Lab03-JUnit for Unit Test.zip",
-        "../temp"
-    )
+    homeworks_file_path = "../data/Lab03-JUnit for Unit Test.zip"  # 源文件路径
+    homeworks_result_dir = "../temp"  # 父目录存储结果
 
-    with UnZip(
-        "/Users/node/Developer/Python/NormLab/data/Lab03-JUnit for Unit Test.zip",
-        "../temp"
-    ) as unzip:
-        unzip.extract()
+    normLab = NormLab(homeworks_file_path, homeworks_result_dir)  # 创建系统对象
+
+    normLab.extract_source_homework()  # 解压作业文件
--- test/test_normlab.py
+++ test/test_normlab.py
@@ -1,4 +1,16 @@
+import pytest
+import os
+
 from normlab import NormLab
+
+
+@pytest.fixture(scope="module")
+def normlab_obj():
+    homeworks_file_path = "../data/Lab03-JUnit for Unit Test.zip"  # 源文件路径
+    homeworks_result_dir = "../temp"  # 父目录存储结果
+    students_list_path = "../data/students_list.csv"
+
+    return NormLab(homeworks_file_path, homeworks_result_dir, students_list_path)  # 创建系统对象
 
 
 def test_get_lab_id():
@@ -7,3 +19,13 @@ def test_get_lab_id():
     )
 
     assert lab.get_lab_id() == "Lab03"
+
+
+def test_extract_source_homework(normlab_obj: NormLab):
+    normlab_obj.extract_source_homework()
+
+
+def test_read_students_list(normlab_obj: NormLab):
+    normlab_obj.read_students_list("../data/students_list.csv")
+    assert normlab_obj.get_students()['L201926630103'].get_full_name() == "Doma Victor"
+    assert not normlab_obj.get_students()['L201926630103'].get_full_name() == "Node Sans"
