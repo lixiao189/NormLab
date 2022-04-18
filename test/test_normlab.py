b012ebde1bf5f1baf06b9e0b613a90b51018cc62
Node Sans <node189@icloud.com>
2022-04-18 16:43:03 +0800

    测试代码完成

--- test/test_normlab.py
+++ test/test_normlab.py
@@ -10,12 +10,13 @@ from normlab import NormLab
 
 @pytest.fixture(scope="module")
 def normlab_obj():
-    homeworks_file_path = "../data/Lab03-JUnit for Unit Test.zip"  # 源文件路径
+    homeworks_file_path = "../data/Lab03-JUnit for Unit Test.zip-JUnit for Unit Test.zip"  # 源文件路径
     homeworks_result_dir = "../result"  # 父目录存储结果
     students_list_path = "../data/students_list.csv"
 
-    with student.CSVStudentRepo(students_list_path) as repo:
-        normlab_obj = NormLab(homeworks_file_path, homeworks_result_dir, repo)
+    with student.CSVStudentRepo(students_list_path) as repo, student.CSVSimilarReporter(
+            homeworks_result_dir) as similar_reporter:
+        normlab_obj = NormLab(homeworks_file_path, homeworks_result_dir, repo, similar_reporter)
     return normlab_obj  # 创建系统对象
 
 
