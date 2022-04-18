e201a60252d6886a3d64ca9d0d7907661dd4a28a
Node Sans <node189@icloud.com>
2022-04-18 14:03:18 +0800

    添加并查集测试文件

--- test/test_student.py
+++ test/test_student.py
@@ -1,3 +1,4 @@
+import student
 from student import CSVStudentRepo
 
 
@@ -6,3 +7,19 @@ def test_get_student() -> None:
 
     with CSVStudentRepo(csv_path) as repo:
         assert repo.get_student("L201926630104").get_full_name() == "Chapata Tinashe"
+
+
+def test_similar_group() -> None:
+    """
+    测试并查集
+    """
+    csv_path = "../data/students_list.csv"
+
+    with CSVStudentRepo(csv_path) as repo:
+        similar_group = student.SimilarGroup(repo)
+
+    similar_group.union("L201926630114", "L201926630133")
+    similar_group.union("L201926630126", "L201926630133")
+
+    assert similar_group.find("L201926630114") == similar_group.find("L201926630126")
+    assert not similar_group.find("L201926630134") == similar_group.find("L201926630133")
